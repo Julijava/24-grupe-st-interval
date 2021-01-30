21fd7b80118bd2ee75c9cf67189a9b193bf717d9
Julija <varnaite.j@gmail.com>
2021-01-30 22:56:41 +0200

    Set interval

--- js/components/counter.js
+++ js/components/counter.js
@@ -1,5 +1,30 @@
-function counter() {
+/*
+REIKALAVIMAI:
+- rasti vieta/elementa
+- istraukti skaiciu
+- kas sekunde ta skaiciu mazinti iki nulio
+- pasiekus nuli - parasyti FINITO
+*/
 
+let number = 0;
+
+function counter(selector) {
+    const DOM = document.querySelector(selector);
+    let number = parseInt(DOM.innerText);
+
+    const timer = setInterval(function () {
+        showMeNumber(number);
+        number--;
+        if (number < 0) {
+            clearInterval(timer);
+
+        }
+    }, 1000);
+
+}
+
+function showMeNumber(num) {
+    console.log(num);
 }
 
 export { counter }
